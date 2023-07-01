896c910b75997355fc18c4991447ec6a54c79de8
Yajat Malhotra <68477362+iamyajat@users.noreply.github.com>
2023-07-02 04:31:24 +0530

    feat: login

--- web/app/layout.js
+++ web/app/layout.js
@@ -1,9 +1,5 @@
 "use client"
-import './globals.css'
-import { Inter } from 'next/font/google'
 import { ChakraProvider } from "@chakra-ui/react"
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
