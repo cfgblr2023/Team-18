fe89db03b2449b815af4c6c1fd58942383265543
Yajat Malhotra <68477362+iamyajat@users.noreply.github.com>
2023-07-02 08:55:40 +0530

    another final commit :")

--- web/components/Slidebar.js
+++ web/components/Slidebar.js
@@ -31,10 +31,8 @@ export default function Carousel() {
 
   // These are the images used in the slide
   const cards = [
-    'https://images.unsplash.com/photo-1612852098516-55d01c75769a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-    'https://images.unsplash.com/photo-1627875764093-315831ac12f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-    'https://images.unsplash.com/photo-1571432248690-7fd6980a1ae2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=900&q=60',
-  ];
+    'https://www.brookings.edu/wp-content/uploads/2017/11/cue_group-work_bangladesh_001.jpg'
+    ];
 
   return (
     <Box
